5f84e1e625a04672b617030011f05d13ae613b3c
Adrienne <adrienneeaston@gmail.com>
2020-10-13 23:12:20 -0700

    jquery adds target on click

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 'use strict'
 
+function hello() {
+  console.log('Hello world!');
+}
+
+hello();
+
+$(document).ready(function() {
+  $("button").click(function() {
+    $("a").attr("target", "_blank");
+  });
+});
