4d981e3ac53f224380627998215620b8e04ee9c7
Adrienne <adrienneeaston@gmail.com>
2020-10-14 00:02:24 -0700

    updated some naming

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 'use strict'
 
-function hello() {
-  console.log('Hello world!');
-}
-
-hello();
-
 $(document).ready(function() {
-  $("#target").on("click", function() {
-    $(".notarget").attr("target", "_blank");
+  $("#target-button").on("click", function() {
+    $(".no-target").attr("target", "_blank");
   });
 
-  $("#default").on("click", function() {
-    $(".notarget").removeAttr("target", "_blank");
+  $("#default-button").on("click", function() {
+    $(".no-target").removeAttr("target", "_blank");
   });
 });
