4dabece7d326aeb3b12d34e84eafbcd1031897ee
Adrienne <adrienneeaston@gmail.com>
2020-10-15 19:33:45 -0700

    created variables

--- app.js
+++ app.js
@@ -2,10 +2,16 @@
 
 $(document).ready(function() {
   $("#target-button").on("click", function() {
-    $(".no-target").attr("target", "_blank");
+    var addTarget = $(".no-target");
+    addTarget.attr("target", "_blank");
   });
 
   $("#default-button").on("click", function() {
-    $(".no-target").removeAttr("target", "_blank");
+    var removeTarget = $(".no-target");
+    removeTarget.removeAttr("target", "_blank")
   });
 });
+
+// target-button - id for button that changes 'a' tags to open into a new tab
+// default-button - id for button that changes 'a' tags back to pre-set behavior
+// no-target - class for all buttons with default behavior of opening in same tab
