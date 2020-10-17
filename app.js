c7f6be9281d86c3bdcec00c3914ab51c84b8ce59
Adrienne <adrienneeaston@gmail.com>
2020-10-17 16:38:19 -0700

    actions refactored to functions

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 'use strict'
 
-$(document).ready(function() {
-  $("#target-button").on("click", function() {
+function changeLinkBehavior() {
+  $(document).ready(function() {
     var addTarget = $(".no-target");
     addTarget.attr("target", "_blank");
   });
+}
 
-  $("#default-button").on("click", function() {
+function revertLinkBehavior() {
+  $(document).ready(function() {
     var removeTarget = $(".no-target");
     removeTarget.removeAttr("target", "_blank")
   });
-});
+}
 
-// target-button - id for button that changes 'a' tags to open into a new tab
-// default-button - id for button that changes 'a' tags back to pre-set behavior
 // no-target - class for all buttons with default behavior of opening in same tab
