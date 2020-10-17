d417078d066a89371ee779018cf5ca5596de324d
Adrienne <adrienneeaston@gmail.com>
2020-10-17 16:48:14 -0700

    made more dry

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 'use strict'
 
+// no-target - class for all buttons with default behavior of opening in same tab
+
 function changeLinkBehavior() {
-  $(document).ready(function() {
     var addTarget = $(".no-target");
     addTarget.attr("target", "_blank");
-  });
 }
 
 function revertLinkBehavior() {
-  $(document).ready(function() {
     var removeTarget = $(".no-target");
     removeTarget.removeAttr("target", "_blank")
-  });
 }
-
-// no-target - class for all buttons with default behavior of opening in same tab
